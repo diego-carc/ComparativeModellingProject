01e76f37a1ccc59fa693fead95cf6dd9a360f137
Diego Carmona Campos <diegocar@lcg.unam.mx>
2024-03-11 11:40:26 -0600

    Made Sequence modelling

---
+++ SequenceModelling/seq_modelling.py
@@ -0,0 +1,18 @@
+# Homology modeling with multiple templates
+from modeller import *              # Load standard Modeller classes
+from modeller.automodel import *    # Load the automodel class
+
+log.verbose()    # request verbose output
+env = environ()  # create a new MODELLER environment to build this model in
+
+# directories for input atom files
+env.io.atom_files_directory = ['.', '../atom_files']
+
+a = automodel(env,
+              alnfile  = 'P11018_templates.pir', # alignment filename
+              knowns   = ('2wv7A', '2x8jA', '6f9mA', '7xr8A', '7y6mC'),     # codes of the templates
+              sequence = 'P11018')               # code of the target
+a.starting_model= 1                 # index of the first model
+a.ending_model  = 2                 # index of the last model
+                                    # (determines how many models to calculate)
+a.make()                            # do the actual homology modeling
